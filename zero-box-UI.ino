74c3dcbf7f529fb3e54c67762c8033127d3a9f45
gl0p <96223996+gl0p@users.noreply.github.com>
2021-12-16 02:11:40 -0500

    Update zero-box-UI.ino

--- zero-box-UI.ino
+++ zero-box-UI.ino
@@ -5,7 +5,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-UTFT lcd(ILI9341_16,38,39,40,41);
+UTFT lcd(ILI9341_16,38,39,40,41);   // CHANGE TO YOUR DISPLAY //
 
 extern uint8_t SmallFont[];
 extern uint8_t BigFont[];
